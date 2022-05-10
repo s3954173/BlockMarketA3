b4237f7fddf91294174524cd02839e00216d782e
sviozrsx29 <s3935418@student.rmit.edu.au>
2022-05-10 16:52:24 +1000

    more visual updates

--- website/src/components/skills/Skills.jsx
+++ website/src/components/skills/Skills.jsx
@@ -3,8 +3,8 @@ import './skills.css'
 const Skills = () => {
   return (
     <div id="skills" className="skills">
-      <h1 className="sectionheading__text">Skills.</h1>
-      <h2 className="sectionheading__subtext">What talent do we need for our project?</h2>
+      <h1 id="titletext-skills" className="sectionheading__text">Skills.</h1>
+      <h2 id="subtext-skills" className="sectionheading__subtext">What talent do we need for our project?</h2>
     <div className="skills__text">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Viverra nam libero justo laoreet sit. Turpis egestas sed tempus urna et pharetra. Egestas erat imperdiet sed euismod nisi porta lorem mollis. Faucibus in ornare quam viverra. Pellentesque nec nam aliquam sem et tortor consequat id porta. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Nisi scelerisque eu ultrices vitae auctor eu. Magna sit amet purus gravida quis blandit turpis cursus in. Purus sit amet luctus venenatis lectus magna. Enim praesent elementum facilisis leo vel fringilla est ullamcorper. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit. Consectetur adipiscing elit ut aliquam purus sit amet. Vitae et leo duis ut diam. Maecenas accumsan lacus vel facilisis volutpat. Tincidunt arcu non sodales neque. Velit ut tortor pretium viverra suspendisse potenti nullam ac tortor. Id semper risus in hendrerit gravida rutrum quisque non. Diam vel quam elementum pulvinar etiam.
 
--- website/src/components/tools/Tools.jsx
+++ website/src/components/tools/Tools.jsx
@@ -3,8 +3,8 @@ import './tools.css'
 const Tools = () => {
   return (
     <div id="tools" className="tools">
-      <h1 className="sectionheading__text">Tools.</h1>
-      <h2 className="sectionheading__subtext">Tracking our repository progress.</h2>
+      <h1 id="titletext-tools" className="sectionheading__text">Tools.</h1>
+      <h2 id="subtext-tools" className="sectionheading__subtext">Tracking our repository progress &nbsp; //</h2>
     <div className="tools__text">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Viverra nam libero justo laoreet sit. Turpis egestas sed tempus urna et pharetra. Egestas erat imperdiet sed euismod nisi porta lorem mollis. Faucibus in ornare quam viverra. Pellentesque nec nam aliquam sem et tortor consequat id porta. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Nisi scelerisque eu ultrices vitae auctor eu. Magna sit amet purus gravida quis blandit turpis cursus in. Purus sit amet luctus venenatis lectus magna. Enim praesent elementum facilisis leo vel fringilla est ullamcorper. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit. Consectetur adipiscing elit ut aliquam purus sit amet. Vitae et leo duis ut diam. Maecenas accumsan lacus vel facilisis volutpat. Tincidunt arcu non sodales neque. Velit ut tortor pretium viverra suspendisse potenti nullam ac tortor. Id semper risus in hendrerit gravida rutrum quisque non. Diam vel quam elementum pulvinar etiam.
 
