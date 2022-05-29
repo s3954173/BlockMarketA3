0983358a51af885d33b34aa44ce9f912f14389c2
Russell <s3935418@student.rmit.edu.au>
2022-05-29 23:09:14 +1000

    update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,4 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css'
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-     <HashRouter>
-       <App />
-     </HashRouter>
-   </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
