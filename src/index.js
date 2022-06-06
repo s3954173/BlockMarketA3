c968a70c4986f59dcebbc6f1a0a3e524739dce02
sviozrsx29 <s3935418@student.rmit.edu.au>
2022-06-06 15:29:17 +1000

    import router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css'
+import Router from 'react-router-dom';
 ReactDOM.render(
 <Router basename='/BlockMarketA3'>
 <App />
